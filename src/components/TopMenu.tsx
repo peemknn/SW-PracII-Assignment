1b9868c4e225da22591033addcf4b3205a792040
Peemknn <kunanontpeem333@gmail.com>
2023-09-14 19:08:18 +0700

    edit some topmenu design

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -4,7 +4,7 @@ import TopMenuItem from './TopMenuItem'
 
 export default function TopMenu() {
     return (
-        <div className='h-12 fixed bg-white left-0 top-0 right-0 z-30 border border-y-1 border-solid border-slate-400 flex flex-row-reverse '>
+        <div className='h-14 fixed bg-white left-0 top-0 right-0 z-30 border border-y-1 border-solid border-slate-400 flex flex-row-reverse '>
             <Image src={'/img/logo.png'} className='h-[100%] w-[100px]' alt='logo'
                 width={0} height={0} sizes='100vh' />
             <TopMenuItem title='Booking' pageRef='/booking' />
--- src/components/TopMenuItem.tsx
+++ src/components/TopMenuItem.tsx
@@ -3,7 +3,7 @@ import styles from "./topmenu.module.css"
 
 export default function TopMenuItem({ title, pageRef }: { title: string, pageRef: string }) {
     return (
-        <Link href={pageRef} className='w-[120px] text-center my-auto text-xs text-slate-400 font-sans hover:text-black active:text-black '>
+        <Link href={pageRef} className='w-[150px] text-center my-auto text-xs text-black hover:text-black active:text-black '>
             {title}
         </Link>
     )
