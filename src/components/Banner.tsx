b7a5d730943236faa99e2f4e7f51e3af9e4dfe42
Peemknn <kunanontpeem333@gmail.com>
2023-10-06 14:32:45 +0700

    edit some fonts

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -13,8 +13,8 @@ export default function Banner() {
     <div className='block p-5 m-0 w-[100vw]  h-[60vh] relative' onClick={()=>{ setIndex(index+1)}}>
           <Image src={covers[index%4]} alt="cover" fill={true} objectFit="cover" />
           <div className='absolute p-4 m-5 rounded-lg top-1 left-1 text-center bg-white flex flex-col '>
-              <h1 className='text-3xl'>ถึงแม้จะหมดยุคโควิดแล้ว แต่วัคซีนก็ยังจำเป็นสำหรับทุกคน</h1>
-              <a className='text-xl' href="#">จองคิวฉีดวัคซีน ณ โรงพยาบาลใกล้บ้านคุณได้ที่นี่</a>
+              <h1 className='text-3xl font-primary'>ถึงแม้จะหมดยุคโควิดแล้ว แต่วัคซีนก็ยังจำเป็นสำหรับทุกคน</h1>
+              <a className='text-xl font-primary' href="#">จองคิวฉีดวัคซีน ณ โรงพยาบาลใกล้บ้านคุณได้ที่นี่</a>
           </div>
           <button
         className="bg-white font-primary text-lg text-black border border-cyan-600 font-semibold py-2 px-2 m-2 rounded z-30 absolute bottom-0 right-0
--- src/components/TopMenuItem.tsx
+++ src/components/TopMenuItem.tsx
@@ -3,7 +3,7 @@ import styles from "./topmenu.module.css"
 
 export default function TopMenuItem({ title, pageRef }: { title: string, pageRef: string }) {
     return (
-        <Link href={pageRef} className='w-[150px] text-center my-auto text-xs text-black hover:text-black active:text-black '>
+        <Link href={pageRef} className='w-[150px] font-primary text-center my-auto text-xs text-black hover:text-black active:text-black '>
             {title}
         </Link>
     )
